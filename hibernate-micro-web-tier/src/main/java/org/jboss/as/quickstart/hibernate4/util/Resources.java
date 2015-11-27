ca9aeb81fe16fbfd2bcd1986c8687cb2cc52e0d3
Kenneth Evensen <kevensen@redhat.com>
2015-11-27 16:31:38 -0500

    Cleaned up Resources.java.  Added pom files

--- hibernate-micro-web-tier/src/main/java/org/jboss/as/quickstart/hibernate4/util/Resources.java
+++ hibernate-micro-web-tier/src/main/java/org/jboss/as/quickstart/hibernate4/util/Resources.java
@@ -16,41 +16,14 @@
  */
 package org.jboss.as.quickstart.hibernate4.util;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.List;
-import java.util.Properties;
 import java.util.logging.Logger;
 
-import javax.annotation.ManagedBean;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
-import org.jboss.as.quickstart.hibernate4.model.Member;
-import org.jboss.as.quickstart.hibernate4.service.MemberListProducerService;
-import org.jboss.as.quickstart.hibernate4.service.MemberRegistrationService;
-
-/**
- * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
- * 
- * <p>
- * Example injection on a managed bean field:
- * </p>
- * 
- * <pre>
- * &#064;Inject
- * private EntityManager em;
- * </pre>
- */
-@ManagedBean
 public class Resources {
-
 
     @Produces
     public Logger produceLog(InjectionPoint injectionPoint) {
@@ -64,65 +37,18 @@ public class Resources {
     }
     
     @Produces
-    public MemberRegistrationService produceMemberRegistrationService()
-    {
-    	try
-    	{
-	    	Properties jndiProps = new Properties();
-	   	
-	    	
-	    	jndiProps.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-	    	
-	    	final Context ctx = new InitialContext(jndiProps);
-	   	
-	    	MemberRegistrationService service = (MemberRegistrationService) ctx.lookup("ejb:/jboss-hibernate4-micro-business/MemberRegistration!"+MemberRegistrationService.class.getName());
-	    	return service;
-    	}
-    	catch(Exception e)
-    	{
-    		System.err.println(e);
-    	}
-    	return null;
-    	
-    }
-    
-    @Produces
-    @Named
-    public List<Member> getMembers() throws NamingException
-    {
-
-    	Properties jndiProps = new Properties();
-   	
-    	
-    	jndiProps.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-    	
-    	final Context ctx = new InitialContext(jndiProps);
-   	
-    	MemberListProducerService service = (MemberListProducerService) ctx.lookup("ejb:/jboss-hibernate4-micro-business/MemberListProducer!"+MemberListProducerService.class.getName()+"?stateful");
-    	return service.getMembers();
-    	
-    	
-    }
-    
-    @Produces
     @Named
     public String getHostIpAddress()
     {
-    	
     	InetAddress ip;
-		try {
-			ip = InetAddress.getLocalHost();
-			return ip.getHostAddress();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "IP Not Found";
-    	
+	try {
+		ip = InetAddress.getLocalHost();
+		return ip.getHostAddress();
+	} catch (UnknownHostException e) {
+		e.printStackTrace();
+	}
+	return "IP Not Found";
     }
-    
-
-    
-
 
 }
+
